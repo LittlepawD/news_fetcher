61f0eb850d412cda31ba8d1b810fcbc057f342bb
LittlepawD <dbodnr37@gmail.com>
2020-10-22 20:49:24 +0200

    keys

--- key_writter.py
+++ key_writter.py
@@ -1,9 +1,12 @@
 """
-Tool for pickling api keys, in format: N_KEY, M_KEY, O_KEY.
+Warning! Do not add this to commits anymore!!
+
+Tool for pickling api keys, in format: news api key, bot api key, O_KEY.
 """
 import pickle
 
-keys = ("empty","empty","empty")
+# keys are not real - see your own api tokens and insert here. 
+keys = ("da1e5458baf040e99e8fb951a978a719","820678802:AAF-Yre6EK5Jyjeqi9EYjMmsd2WS1nr2TiY","empty")
 
 with open("keys.bin", "wb") as f:
     pickle.dump(keys, f)
