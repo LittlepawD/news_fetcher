752a39fe1bbdbc311cba2ce74a38094551d4e974
LittlepawD <dbodnr37@gmail.com>
2020-10-22 20:22:45 +0200

    v 1.0.0

--- main.py
+++ main.py
@@ -1,47 +1,73 @@
 import requests as rq
 import pprint as pp
-import pickle
+import pickle, telebot, datetime
 
 # Load News Api key, Mail keys from pickled tuple:
 with open("keys.bin", "rb") as f:
     N_KEY, T_KEY, O_KEY = pickle.load(f)
 
-URL = "https://newsapi.org/v2/"
-SOURCES = """abc-news,
-            al-jazeera-english,
-            associated-press,
-            bbc-news,
-            cbc-news,
-            cnn,
-            independent,
-            reddit-r-all,
-            reuters,
-            the-washington-post,
-            time,
-            news-com-au,
-            nbc-news"""
+class News_fetcher:
 
-def get_sources():
-    data = {"apiKey": N_KEY,
-        "language": "en"}
-    data["category"] = "general"
-    resp = rq.get(URL + "sources", data)
-    if resp.ok: return resp 
-    else: return resp.status_code
+    def __init__(self, api_key):
+        self._KEY = api_key
+        self.URL = "https://newsapi.org/v2/"
+        self.SOURCES = """abc-news,
+                al-jazeera-english,
+                associated-press,
+                bbc-news,
+                cbc-news,
+                cnn,
+                independent,
+                reddit-r-all,
+                reuters,
+                the-washington-post,
+                time,
+                news-com-au,
+                nbc-news"""
 
-def get_news():
-    data = {
-        "apiKey": N_KEY,
-        #"sources": SOURCES,
-        "q": "((czech republic) OR czechia OR prague OR brno) AND -football",
-        "from":  "10-20-2020"
-    }
-    resp = rq.get(URL + "everything", data)
-    if resp.ok: return resp 
-    else: return resp.status_code
+    def get_sources(self):
+        data = {"apiKey": self._KEY,
+            "language": "en",
+            "category": "general"}
+        resp = rq.get(self.URL + "sources", data)
+        if resp.ok: return resp 
+        else: return resp.status_code
 
-pp.pprint(news)
+    # Poznamky ke zdrojum - independent má news digest, s minutovymi zpravami, nahovno
+    # reuters vydava zpravy 2x, pro .com a indii
+    def get_news(self):
+        yesterday = datetime.datetime.now() - datetime.timedelta(days=1, hours=1)
+        data = {
+            "apiKey": self._KEY,
+            "sources": self.SOURCES,
+            "q": "((czech republic) OR czechia OR prague OR brno) AND (-football -digest)",
+            "excludeDomains": "",
+            "from":  yesterday.isoformat(timespec="minutes")
+        }
+        resp = rq.get(self.URL + "everything", data)
+        if resp.ok: return resp 
+        else: return resp.status_code
+
+class NewsTeleBot(telebot.TeleBot):
+    def send_article(self, article):
+        channel = "@SnepsCzechNews"
+        self.send_message(channel, f"<b>{article['title']}</b>\n\n{article['description']}\n{article['url']}", parse_mode="html")
+        # self.send_message(channel, f"<b>{article['title']}</b>\n\n{article['url']}", parse_mode="html", disable_web_page_preview=False)
+
 
 if __name__ == "__main__":
-    
-    news = get_news().json()
+    # init bot, fetcher:
+    bot = NewsTeleBot(T_KEY)
+    fetcher = News_fetcher(N_KEY)
+
+    # Get and parse news:
+    news = fetcher.get_news().json()
+    print(f"Fetcher got {news['totalResults']} news.")
+    counter = 0
+    for new in news["articles"]:
+        if "reuters india" not in new["title"].lower():
+            pp.pprint(new["title"])
+            bot.send_article(new)
+            counter += 1
+
+    print(f"\nSent {counter} news.")
