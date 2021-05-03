a4f4a70df0154f44e1e8b23e6eb53002b6b56586
LittlepawD <dbodnr37@gmail.com>
2021-05-03 12:57:49 +0200

    add tešt to check crypto module

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ Tests connection to telegram via TelegramBotAPI
 """
 
 import main as m
-
 dev_chat_id = 449951283
 
 if __name__ == "__main__":
     bot = m.NewsTeleBot(m.T_KEY)
-    bot.send_message(str(dev_chat_id), "Test succesfull")
+    fetcher = m.News_fetcher("key")
+    crypto_cli = m.crypto_movers.Client()
+    crypto_cli.get_btc_price("EUR")
+    # bot.send_message(str(dev_chat_id), "Test succesfull")
     print("Test finished without exception.")
