d85917148ee1befe70a97366527b16926434485d
LittlepawD <dbodnr37@gmail.com>
2021-05-04 13:10:20 +0200

    update test

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     bot = m.NewsTeleBot(m.T_KEY)
     fetcher = m.News_fetcher("key")
     crypto_cli = m.crypto_movers.Client()
-    crypto_cli.get_btc_price("EUR")
+    crypto_cli.get_crypto_price("BTC", "EUR")
     bot.send_message(str(dev_chat_id), "Test succesfull")
     print("Test finished without exception.")
